56438d3b17cba554684fa33316b2acbdfc4e3ed1
ShubhangiK120 <shubhangi.khangar@indiumsoft.com>
2021-12-30 23:52:55 +0530

    Assignment7

---
+++ studentsDetails/StudentsDisplay.java
@@ -0,0 +1,15 @@
+package students;
+
+public class StudentsDisplay
+{
+
+	public static void main(String[] args) 
+	{
+      Student std=new Student();
+      
+      std.setName("Amit");
+      String Name=std.getName();
+      System.out.println(Name);
+	}
+
+}
